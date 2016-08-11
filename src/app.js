f4c1fe3c4e5ec35a289e49d2f518728feab95bb7
chenrunsheng <chen742068070@163.com>
2016-08-11 18:01:48 +0800

    webpack-react-app构建优化

---
+++ src/app.js
@@ -0,0 +1,10 @@
+'use strict';
+// Import CSS
+import './sass/master.scss';
+
+// Import React and JS
+import React from 'react';
+import HelloBox from './js/HelloBox';
+
+// Render!
+React.render(<HelloBox />, document.getElementById('app'));
